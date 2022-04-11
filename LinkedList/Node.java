65b26e8bcd842c44457ad3fac78f55fae9ae09b9
unknown <Maxdesouza2005@gmail.com>
2022-04-11 22:25:31 +0800

    11/4/22 work

--- LinkedList/Node.java
+++ LinkedList/Node.java
@@ -8,6 +8,7 @@
 public class Node {
     private int value; 
     private Node next;
+    
 
     Node(int value) {
         this.value = value;
@@ -39,22 +40,81 @@ public class Node {
         return (next != null);
     }
     
-    public Node mergeList(Node LinkedList1, Node LinkedList2){
+    public void printList() {
+        Node temp = this;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+        }
+    }
+    public static void main(String[] args) {
+    
+    
+        Node LinkedList1 = new Node(1);
+        Node head1 = LinkedList1;
+        LinkedList1.setNext(new Node(3));
+        LinkedList1= LinkedList1.getNext();
+        LinkedList1.setNext(new Node(5));
+        LinkedList1= LinkedList1.getNext();
+        LinkedList1.setNext(new Node(7));
+        LinkedList1= LinkedList1.getNext();
+        LinkedList1.setNext(new Node(9));
+        
+        
+        Node LinkedList2 = new Node(2);
+        Node head2 = LinkedList2;
+        LinkedList2.setNext(new Node(2));
+        LinkedList2= LinkedList2.getNext();
+        LinkedList2.setNext(new Node(4));
+        LinkedList2= LinkedList2.getNext();
+        LinkedList2.setNext(new Node(6));
+        LinkedList2= LinkedList2.getNext();
+        LinkedList2.setNext(new Node(8));
+        LinkedList1= LinkedList1.getNext();
+        LinkedList1.setNext(new Node(10));
+        
+        System.out.println("");
+        head1.printList();
+        System.out.println("");
+        head2.printList();
+        System.out.println("");
+        Node mergedlist = mergeList(head1,head2);
+        mergedlist.printList();
+    }
+    public static Node mergeList(Node LinkedList1, Node LinkedList2){
         Node mergeList = new Node();
+        Node mergehead = mergeList;
         while (LinkedList1.hasNext() && LinkedList2.hasNext()) {
+            
             if (LinkedList1.getValue() <= LinkedList2.getValue()) {
                 mergeList.next = LinkedList1;
-                LinkedList1 = LinkedList1.next;
+                
+                LinkedList1 = LinkedList1.getNext();
+                LinkedList1.printList();
             }
  
             else {
-                mergeList.next = LinkedList2;
-                LinkedList2 = LinkedList2.next;
+                mergeList.next = LinkedList2;           
+                
+                LinkedList2 = LinkedList2.getNext();
+                LinkedList2.printList();
             }
             mergeList = mergeList.next;
         }
-        return mergeList.next;
+        boolean a;
+        if(LinkedList1 == null){
+             a = true;
+        }else{
+             a = false;
         }
-        
+        if(a == true){
+            mergeList.next = LinkedList2;
+        }
+        else if(a == false){
+            mergeList.next = LinkedList1;
+        }
+        return mergehead.next;
+        }
+      
      }
 
