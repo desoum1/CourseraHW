6bd462fb9a4183198c68967508aa4684ba3cdeac
unknown <Maxdesouza2005@gmail.com>
2022-06-26 13:02:33 +0800

    matrix sum

---
+++ Matrixsum/NumMatrix.java
@@ -0,0 +1,19 @@
+class NumMatrix {
+
+    public NumMatrix(int[][] matrix) {
+        
+    }
+    
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        while(int x =0; x < col2; x++ ){
+            while(in y =0; y< row2; y++){
+                
+            }}
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
