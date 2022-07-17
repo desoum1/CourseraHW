45c926507f599ca9bf1fd633dba42e169c8d5871
unknown <Maxdesouza2005@gmail.com>
2022-07-17 13:00:41 +0800

    4 times for loop needs to be done

--- Matrixsum/NumMatrix.java
+++ Matrixsum/NumMatrix.java
@@ -2,7 +2,19 @@ class NumMatrix {
    int [][] mymatrix;
     public NumMatrix(int[][] matrix) {
         mymatrix = matrix;
+       
+        int length = matrix.length;
+        int width = matrix[0].length;
+        
+        for (int a = 0;a <= width-1; a++){
+            for(int b = 0;b <= length-1; b++) {
+                System.out.println(mymatrix[a][b]);
+                for()
+        }
     }
+        
+    }
+    
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
         int matrixsum = 0;
