cda14cfa0596a8d159a53d513ab6c143323b5dea
unknown <Maxdesouza2005@gmail.com>
2022-07-24 12:59:16 +0800

    24/7 work

--- Matrixsum/NumMatrix.java
+++ Matrixsum/NumMatrix.java
@@ -1,31 +1,39 @@
 class NumMatrix {
    int [][] mymatrix;
+    int length;
+    int width;
     public NumMatrix(int[][] matrix) {
         mymatrix = matrix;
        
-        int length = matrix.length;
-        int width = matrix[0].length;
+       length = matrix.length;
+        width = matrix[0].length;
         
         for (int a = 0;a <= width-1; a++){
             for(int b = 0;b <= length-1; b++) {
                 System.out.println(mymatrix[a][b]);
-                for()
         }
     }
-        
-    }
+}
+    
     
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int matrixsum = 0;
-        for(int y = row1; y <= row2; y++){
-             for(int x = col1; x <= col2; x++){
-                System.out.println(mymatrix[y][x]);
-                matrixsum += mymatrix[y][x];
-            }
-       }
-    return matrixsum;
-}
+        for(int y = 0; y < length-1; y++){
+            int prev = 0;
+            for(int x = 0;  x < width-1; x++){
+                if(x == 0){
+                    sumregion[x][y] = NumMatrix[x][y] + prev + NumMatrix[x][y-1] + prev + 0;
+                    //first row summary takes all left val from NumMatrix
+                }
+                sumregion[x][y] = NumMatrix[x][y] + prev + NumMatrix[x][y-1];
+                    //take above in sumreg and all left val from NumMatrix
+                    System.out.println(sumregion[x][y]);
+                prev += NumMatrix[x][y];
+                
+                //helps with sum of previous matrix previous
+             }
+        }
+    }            
 }
 
 /**
