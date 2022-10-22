c4ff793ca0a2ea3fb6d91c0352198f80eb2e6451
unknown <Maxdesouza2005@gmail.com>
2022-10-22 13:35:51 -0700

    not sure how to fix

---
+++ OOP/1d to 2d array/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        //m is row
+        //n is collum
+        new array = 2darray[m][n];
+        for (int z = 0; z< int[].length; z++)
+            for (int x = 0; x < m; x++){
+                for (int y = 0; y < n; y++)
+                    if (2darray[m][n] == null)
+                        2darray[m][n] = int[x];
+            }
+
+        }
+    }
