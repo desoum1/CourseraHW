eb19258af70020699e1488b571493d5eddfc4028
unknown <Maxdesouza2005@gmail.com>
2022-11-05 19:31:30 -0700

    faster

--- OOP/2d number finder/Solution.java
+++ OOP/2d number finder/Solution.java
@@ -9,3 +9,25 @@ class Solution {
         return false;
     }
 }
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int x = matrix.length - 1;
+        int y = 0;
+        int ycounter = 0;
+        while (x < matrix.length && y < matrix[0].length && x >= 0){
+           
+            if(target == matrix[x][y]){
+                return true;
+            }
+            if (target > matrix[x][y]){
+                y += 1;
+            }
+            else{
+                x -= 1;
+            }
+            
+        }
+        return false;
+        }
+        
+    }
